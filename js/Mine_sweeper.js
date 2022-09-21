2300a3c37abac290f2562e3db8a9a2122d9d9846
Eilon-Salant <114082126+Eilon-Salant@users.noreply.github.com>
2022-09-21 20:13:50 +0300

    day 1 final commit

--- js/Mine_sweeper.js
+++ js/Mine_sweeper.js
@@ -1,11 +1,13 @@
 'use strict'
 
-const BOMB_IMG = '<img src="img/bomb.png">'
+const MINE_IMG = '<img src="img/mine.png">'
 const FLAG_IMG = '<img src="img/flag.png">'
 
 var gBoard
 var gLevel = { SIZE: 4, MINES: 2 }
 var gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
+var gFirstClick = true
+var gTimeInterval
 
 function initGame() {
   gBoard = buildBoard()
@@ -16,9 +18,9 @@ function initGame() {
 function buildBoard() {
   var board = []
 
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < gLevel.SIZE; i++) {
     board[i] = []
-    for (var j = 0; j < 4; j++) {
+    for (var j = 0; j < gLevel.SIZE; j++) {
       board[i][j] = {
         isShown: false,
         isMine: false,
@@ -28,9 +30,6 @@ function buildBoard() {
     }
   }
 
-  setRandomMines(gLevel.MINES, gLevel.SIZE, board)
-
-  setMinesNegsCount(board)
   //   console.table(board)
   return board
 }
@@ -45,8 +44,8 @@ function renderBoard(board) {
       const currCell = board[i][j]
 
       strHTML += `\t<td class="cell cell-${i}-${j}" onmouseup="clickedCell(this, ${i}, ${j})" >\n`
-      if (board[i][j].isMine && board[i][j].isShown) strHTML += BOMB_IMG
-      else if (!board[i][j].isMine) board[i][j].innertext = board[i][j].minesAroundCount
+      if (currCell.isMine && currCell.isShown) strHTML += MINE_IMG
+      else if (!currCell.isMine) currCell.innertext = currCell.minesAroundCount
     }
     strHTML += '\n</tr>'
   }
@@ -55,8 +54,9 @@ function renderBoard(board) {
 }
 
 function gameLost() {
-  for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 4; j++) {
+  gGame.isOn = false
+  for (var i = 0; i < gLevel.SIZE; i++) {
+    for (var j = 0; j < gLevel.SIZE; j++) {
       var elCell = document.querySelector(`.cell-${i}-${j}`)
       var cell = gBoard[i][j]
 
@@ -65,7 +65,7 @@ function gameLost() {
         elCell.classList.add('shown')
       } else if (!cell.isShown && cell.isMine) {
         cell.isShown = true
-        elCell.innerHTML = BOMB_IMG
+        elCell.innerHTML = MINE_IMG
         elCell.classList.add('mine')
       }
 
@@ -76,14 +76,49 @@ function gameLost() {
   }
 }
 
+function checkGameOver() {
+  var count = gLevel.MINES
+  for (var i = 0; i < gLevel.SIZE; i++) {
+    for (var j = 0; j < gLevel.SIZE; j++) {
+      var cell = gBoard[i][j]
+      count++
+      if (count === gLevel.SIZE ** 2) console.log('You win!')
+      if ((cell.isMine && !cell.isMarked) || (!cell.isMine && !cell.isShown)) {
+        return
+      }
+    }
+  }
+}
+
+// doesnt work yet
+
+// function expandShown(board, elCell, cellI, cellJ) {
+//   for (var i = cellI - 1; i <= cellI + 1; i++) {
+//     if (i < 0 || i >= board.length) continue
+//     for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+//       if (j < 0 || j >= board[i].length) continue
+//       if (i === cellI && j === cellJ) continue
+
+//       var cell = board[cellI][cellJ]
+//       if (!cell.minesAroundCount) {
+//         board[i][j].isShown = true
+//         elCell.classList.add('shown')
+//         gGame.shownCount++
+//       }
+//     }
+//   }
+// }
+
 function setRandomMines(numOfMines, gameSize, board) {
   var minesCount = 0
 
   while (minesCount < numOfMines) {
     var cellI = getRandomIntInclusive(0, gameSize - 1)
     var cellJ = getRandomIntInclusive(0, gameSize - 1)
-    if (!board[cellI][cellJ].isMine) {
-      board[cellI][cellJ].isMine = true
+    var cell = board[cellI][cellJ]
+
+    if (!cell.isMine && !cell.isShown) {
+      cell.isMine = true
       minesCount++
     }
   }
@@ -92,59 +127,77 @@ function setRandomMines(numOfMines, gameSize, board) {
 function setMinesNegsCount(board) {
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board.length; j++) {
-      board[i][j].minesAroundCount = countNeighbors(i, j, board)
+      board[i][j].minesAroundCount = countNegsMines(i, j, board)
     }
   }
 }
 
-function countNeighbors(cellI, cellJ, board) {
-  var neighborsCount = 0
+function countNegsMines(cellI, cellJ, board) {
+  var negsMinesCount = 0
 
   for (var i = cellI - 1; i <= cellI + 1; i++) {
     if (i < 0 || i >= board.length) continue
     for (var j = cellJ - 1; j <= cellJ + 1; j++) {
       if (j < 0 || j >= board[i].length) continue
       if (i === cellI && j === cellJ) continue
-      if (board[i][j].isMine) neighborsCount++
+      var cell = board[i][j]
+      if (cell.isMine) negsMinesCount++
     }
   }
-  return neighborsCount
+  return negsMinesCount
 }
 
 function cellMarked(elCell, i, j) {
   var cell = gBoard[i][j]
-  if (cell.isMarked) {
-    cell.isMarked = false
-    elCell.innerHTML = ''
-  } else {
-    cell.isMarked = true
-    elCell.innerHTML = FLAG_IMG
+  if (!cell.isShown) {
+    if (cell.isMarked) {
+      cell.isMarked = false
+      elCell.innerHTML = ''
+      gGame.markedCount--
+    } else {
+      cell.isMarked = true
+      elCell.innerHTML = FLAG_IMG
+      gGame.markedCount++
+    }
   }
 }
 
 function clickedCell(elCell, i, j) {
+  if (!gGame.isOn) return
   var cell = gBoard[i][j]
   var elCell = elCell
 
   elCell.onclick = () => {
     if (cell.isShown || cell.isMarked) return
+
     if (cell.isMine) {
       elCell.classList.add('mine')
-      elCell.innerHTML = BOMB_IMG
+      elCell.innerHTML = MINE_IMG
       gameLost()
     }
     cell.isShown = true
     elCell.classList.add('shown')
+    gGame.shownCount++
 
-    console.log(gBoard)
-    console.log(elCell)
+    // console.log(gBoard)
+    // console.log(elCell)
 
-    if (cell.minesAroundCount) {
-      console.log(cell.minesAroundCount)
+    // doesnt work yet
+    // if (!cell.minesAroundCount && !gFirstClick) expandShown(gBoard, elCell, i, j)
+
+    if (cell.minesAroundCount && !cell.isMine) {
       elCell.innerHTML = cell.minesAroundCount
+    }
+
+    if (gFirstClick) {
+      setRandomMines(gLevel.MINES, gLevel.SIZE, gBoard)
+      setMinesNegsCount(gBoard)
+      gameTimer()
+      gFirstClick = false
     }
   }
   elCell.oncontextmenu = () => {
     cellMarked(elCell, i, j)
   }
+  checkGameOver()
 }
--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,22 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+var elClock = document.querySelector('#clock')
+var gameStartTime = 0 // game-milliseconds;
+var realStartTime = Date.now() // real milliseconds
+
+function gameTimer() {
+  gTimeInterval = setInterval(function () {
+    var gameTime = gameStartTime + (Date.now() - realStartTime)
+    var sec = Math.floor(gameTime / 1000) % 60
+    var min = Math.floor(gameTime / 60000) % 60
+    var hour = Math.floor(gameTime / 3600000) % 24
+    // output in hh:mm:ss format:
+    elClock.textContent = `${padNum(hour)}:${padNum(min)}:${padNum(sec)}`
+  }, 50)
+
+  function padNum(number) {
+    return (number < 10 ? '0' : '') + number
+  }
+}
